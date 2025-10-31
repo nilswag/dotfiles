33afabfe9ed2345b3b4a8222dfd1d7702b8d99af
nilswag <nilswagemaker05@icloud.com>
2025-10-31 01:03:09 +0100

    Added neovim options

---
+++ nvim/.config/nvim/lua/config/options.lua
@@ -0,0 +1,42 @@
+
+local o = vim.opt
+local g = vim.g
+
+o.cursorline = true
+o.scrolloff = 6
+
+o.signcolumn = "yes"
+o.number = true
+
+o.wrap = false
+
+o.undofile = true
+o.backup = false
+o.writebackup = false
+
+o.ignorecase = true
+o.smartcase = true
+o.hlsearch = false
+
+o.autoindent = true
+o.copyindent = true
+o.breakindent = true
+
+o.tabstop = 4
+o.shiftwidth = 4
+o.softtabstop = 4
+o.expandtab = true
+
+o.clipboard = "unnamedplus"
+g.clipboard = {
+	["name"] = "WslClipboard",
+	["copy"] = {
+		["+"] = "clip.exe",
+		["*"] = "clip.exe"
+	},
+	["paste"] = {
+		["+"] = "powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))",
+		["*"] = "powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace('`r', ''))"
+	},
+	["cache_enabled"] = 0
+}
