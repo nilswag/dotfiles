b0927f658471a7afc2cc2ecc35cea8703bd4aa96
nilswag <nilswagemaker05@icloud.com>
2025-11-01 16:12:36 +0100

    idk

--- nvim/.config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/plugins/lsp/nvim-lspconfig.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local on_attach = function(client, bufnr)
             local km = vim.keymap.set
- 
+
             km("n", "gD", vim.lsp.buf.declaration, { desc = "Go to declaration" })
             km("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
             km("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
@@ -14,7 +14,7 @@ return {
             km("n", "K", vim.lsp.buf.hover, { desc = "Hover information" })
             km("n", "<leader>d", vim.diagnostic.open_float, { desc = "Show diagnostic" })
 
-            -- km("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Show code actions" })
+            km("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Show code actions" })
         end
 
         vim.lsp.config("lua_ls", { on_attach = on_attach })
