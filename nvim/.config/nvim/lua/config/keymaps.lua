479f3ba92086f1a717416cd984a32f48fd5a4375
nilswag <nilswagemaker05@icloud>
2025-11-01 20:12:25 +0100

    Changed keymap

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -28,8 +28,8 @@ km("v", "<", "<gv", { desc = "Indent left" })
 km("v", ">", ">gv", { desc = "Indent right" })
 
 -- move selected lines in visual mode
-km("v", "K", ":m '<-2<cr>gv=gv", { desc = "Move selected line up one", silent = true })
-km("v", "J", ":m '>+1<cr>gv=gv", { desc = "Move selected line down one", silent = true })
+km("v", "<a-j>", ":m '>+1<cr>gv=gv", { desc = "Move selected line down one", silent = true })
+km("v", "<a-k>", ":m '<-2<cr>gv=gv", { desc = "Move selected line up one", silent = true })
 
 -- move current line in normal mode
 km("n", "<a-j>", ":m .+1<cr>==", { desc = "Move current line down one", silent = true })
