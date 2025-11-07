eda84be932bbe69bd400cb6f9b0fb79baf231e82
nilswag <nilswagemaker05@icloud.com>
2025-11-07 16:53:48 +0100

    Added nvim-tree

--- nvim/.config/nvim/lua/nilswag/config/remap.lua
+++ nvim/.config/nvim/lua/nilswag/config/remap.lua
@@ -1,10 +1,13 @@
 -- remap.lua
 vim.g.mapleader = " "
+local opts = function(desc)
+  return { noremap = true, silent = true, desc = desc }
+end
 
 -- normal mode
-vim.keymap.set("n", "<leader>pv", ":Ex<cr>", { noremap = true, silent = true, desc = "Open project view" })
+-- vim.keymap.set("n", "<leader>pv", ":Ex<cr>", { noremap = true, silent = true, desc = "Open project view" })
 
 -- visual mode
 
 -- insert mode
-vim.keymap.set("i", "jk", "<esc>", { noremap = true, silent = true, desc = "Shortcut for <esc> key" })
+vim.keymap.set("i", "jk", "<esc>", opts("Shortcut for <esc> key"))
