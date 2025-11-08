99a50a62f921df0f7f598c955dfe36796a8ee30d
nilswag <nilswagemaker05@icloud.com>
2025-11-08 21:59:56 +0100

    Added telescope

--- nvim/.config/nvim/lua/nilswag/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/nilswag/plugins/treesitter.lua
@@ -1,9 +1,9 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
-  branch = 'master', 
-  lazy = false, 
+  "nvim-treesitter/nvim-treesitter",
+  branch = 'master',
+  lazy = false,
   build = ":TSUpdate",
-  
+
   config = function()
     require("nvim-treesitter.configs").setup({
       -- A list of parser names, or "all" (the listed parsers MUST always be installed)
@@ -27,11 +27,11 @@ return {
         -- the name of the parser)
         -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
         disable = function(lang, buf)
-            local max_filesize = 100 * 1024 -- 100 KB
-            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            if ok and stats and stats.size > max_filesize then
-                return true
-            end
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
         end,
 
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
