d7969d232c69d8201b6ec297f4860c22c27784d7
nilswag <nilswagemaker05@icloud.com>
2025-11-08 21:33:54 +0100

    Added basic lsp setup

--- nvim/.config/nvim/lua/nilswag/plugins/lsp.lua
+++ nvim/.config/nvim/lua/nilswag/plugins/lsp.lua
@@ -1,16 +1,50 @@
 return {
   {
     "mason-org/mason.nvim",
+    opts = {}
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
     opts = {
-      ui = {
-        icons = {
-          package_installed = "✓",
-          package_pending = "➜",
-          package_uninstalled = "✗"
-        }
+      ensure_installed = {
+        "lua_ls",
+        "clangd",
+        "vimls"
       }
-    }
+    },
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
   },
 
-  
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local on_attach = function(_, bufnr)
+        local opts = function(desc)
+          return { noremap = true, silent = true, buffer = bufnr, desc = desc }
+        end
+
+        vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, opts("Format buffer"))
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Hover"))
+        vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts("Go to definition"))
+        vim.keymap.set("n", "<leader>gD", vim.lsp.buf.declaration, opts("Go to declaration"))
+        vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, opts("Go to implementation"))
+        vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, opts("Show references"))
+      end
+
+      vim.lsp.config("lua_ls", {
+        on_attach = on_attach
+      })
+
+      vim.lsp.config("clangd", {
+        on_attach = on_attach
+      })
+
+      vim.lsp.config("vimls", {
+        on_attach = on_attach
+      })
+    end
+  }
 }
