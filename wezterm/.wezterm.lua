d1b48a1c9e43a5790320ad93455b1aec67ea2f24
nilswag <nilswagemaker05@icloud.com>
2025-11-08 13:11:16 +0100

    Added basic wezterm config

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "Catppuccin Mocha"
+
+config.font_size = 18
+config.font = wezterm.font("UbuntuMono Nerd Font Mono")
+
+config.enable_tab_bar = false
+
+return config
