c6dc4d8fc39edf6e73bcf55c5318939a270eab32
nilswag <nilswagemaker05@icloud.com>
2025-11-08 21:18:38 +0100

    Added mason

---
+++ nvim/.config/nvim/lua/nilswag/plugins/lsp.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
+        }
+      }
+    }
+  },
+
+  
+}
