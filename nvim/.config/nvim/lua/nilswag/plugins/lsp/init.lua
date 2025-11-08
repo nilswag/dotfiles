299636d7b71ef8beb77de9348aaade6db0358895
nilswag <nilswagemaker05@icloud.com>
2025-11-08 14:09:04 +0100

    Added luasnip because my dumbahh thought that luasnip is only for lua

--- nvim/.config/nvim/lua/nilswag/plugins/lsp/init.lua
+++ nvim/.config/nvim/lua/nilswag/plugins/lsp/init.lua
@@ -16,6 +16,10 @@ return {
     "hrsh7th/cmp-nvim-lsp",
   },
   {
+    "L3MON4D3/LuaSnip",
+    build = "make install_jsregexp"
+  },
+  {
     "hrsh7th/nvim-cmp",
     config = function() require(path .. "nvim-cmp") end
   }
--- nvim/.config/nvim/lua/nilswag/plugins/lsp/nvim-lspconfig.lua
+++ nvim/.config/nvim/lua/nilswag/plugins/lsp/nvim-lspconfig.lua
@@ -1,4 +1,4 @@
-local on_attach = function(bufnr)
+local on_attach = function(client, bufnr)
   local opts = function(desc)
     return { noremap = true, desc = desc }
   end
@@ -6,6 +6,7 @@ local on_attach = function(bufnr)
   vim.keymap.set("n", "K", vim.lsp.buf.hover, opts("Hover"))
   vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts("Go to definition"))
   vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts("Go to implementation"))
+  vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts("Show diagnostics"))
 end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
@@ -17,7 +18,10 @@ vim.lsp.config("lua_ls", {
 
 vim.lsp.config("clangd", {
   on_attach = on_attach,
-  capabilities = capabilities
+  capabilities = capabilities,
+  init_options = {
+    clangdFileStatus = true
+  }
 })
 
 vim.lsp.config("vim_ls", {
