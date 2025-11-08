2bafeaf3563ad508d1aced1eadb8c8aef8b4ec51
nilswag <nilswagemaker05@icloud.com>
2025-11-08 18:01:44 +0100

    Changed wezterm colorscheme

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
-config.color_scheme = "Catppuccin Mocha"
+config.color_scheme = "GruvboxDarkHard"
 
 config.font_size = 18
 config.font = wezterm.font("UbuntuMono Nerd Font Mono")
