96f8f0e02c7b6a736ab8538749a8e48ad092ebbc
nilswag <nilswagemaker05@icloud.com>
2025-11-08 21:07:08 +0100

    Added some window navigation keymaps

--- nvim/.config/nvim/lua/nilswag/config/keymaps.lua
+++ nvim/.config/nvim/lua/nilswag/config/keymaps.lua
@@ -1,6 +1,14 @@
-
+vim.g.mapleader = " "
 local opts = function(desc)
   return { noremap = true, silent = true, desc = desc }
 end
 
 vim.keymap.set("i", "jk", "<esc>", opts("Shortcut for <esc> key"))
+
+vim.keymap.set("n", "<c-h>", "<c-w>h", opts("Move window left"))
+vim.keymap.set("n", "<c-l>", "<c-w>l", opts("Move window right"))
+vim.keymap.set("n", "<c-j>", "<c-w>j", opts("Move window down"))
+vim.keymap.set("n", "<c-k>", "<c-w>k", opts("Move window up"))
+
+vim.keymap.set("n", "<leader>w", ":split<cr>", opts("Split window horizontally"))
+vim.keymap.set("n", "<leader>s", ":vsplit<cr>", opts("Split window vertically"))
