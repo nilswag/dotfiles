8519f0ef82e1794e49ffe791da20693ab6f5fd28
nilswag <nilswagemaker05@icloud.com>
2025-11-10 20:55:25 +0100

    Added transparency in neovim

--- nvim/.config/nvim/lua/nilswag/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/nilswag/plugins/colorscheme.lua
@@ -24,7 +24,7 @@ return {
       palette_overrides = {},
       overrides = {},
       dim_inactive = false,
-      transparent_mode = false,
+      transparent_mode = true,
     })
 
     vim.cmd("colorscheme gruvbox")
