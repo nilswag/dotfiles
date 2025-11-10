82c39658ab31c39b890e177092a1dcb4cff8f633
nilswag <nilswagemaker05@icloud.com>
2025-11-10 16:10:06 +0100

    Removed padding in wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,7 +7,15 @@ config.font_size = 18
 config.font = wezterm.font("UbuntuMono Nerd Font Mono")
 
 config.enable_tab_bar = false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
 
 config.enable_wayland = false
+
 
 return config
