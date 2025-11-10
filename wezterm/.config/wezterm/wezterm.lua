21bdeddaa30b39847aab335447e414ede3a63ace
nilswag <nilswagemaker05@icloud.com>
2025-11-10 21:26:45 +0100

    Added some window split binds

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,8 +1,8 @@
+-- config initialization
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Gruvbox Dark (Gogh)"
-
 config.font_size = 18
 config.font = wezterm.font("UbuntuMono Nerd Font Mono")
 
@@ -13,10 +13,36 @@ config.window_padding = {
   top = 0,
   bottom = 0,
 }
-config.window_background_opacity = 0.75
-config.kde_window_background_blur = true
 
-config.enable_wayland = true
+config.background = {
+  {
+    source = {
+      File = "/home/nils/.config/wezterm/wallpaper.jpg"
+    },
+    hsb = { brightness = 0.075 }
+  }
+}
 
+config.initial_cols = 90
+config.initial_rows = 30
+
+config.leader = { key = "LeftAlt", timeout_milliseconds = 1000 }
+config.keys = {
+  {
+    key = "q",
+    mods = "LEADER",
+    action = wezterm.action.CloseCurrentPane({ confirm = true })
+  },
+  {
+    key = "w",
+    mods = "LEADER",
+    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" })
+  },
+  {
+    key = "s",
+    mods = "LEADER",
+    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" })
+  }
+}
 
 return config
