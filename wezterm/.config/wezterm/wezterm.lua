f79519c046eb242b41476846f82b467d1995e137
nilswag <nilswagemaker05@icloud.com>
2025-11-12 16:21:11 +0100

    Removed hyprland config since fractional scaling bothers me on it and started preparation for tmux in wezterm config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,6 +1,5 @@
 -- config initialization
 local wezterm = require("wezterm")
-local smart_splits = wezterm.plugin.require("https://github.com/mrjones2014/smart-splits.nvim")
 local config = wezterm.config_builder()
 
 config.color_scheme = "Gruvbox Dark (Gogh)"
@@ -28,26 +27,5 @@ config.kde_window_background_blur = true
 
 config.initial_cols = 90
 config.initial_rows = 30
-
-config.leader = { key = "LeftAlt", timeout_milliseconds = 1000 }
-config.keys = {
-  {
-    key = "q",
-    mods = "LEADER",
-    action = wezterm.action.CloseCurrentPane({ confirm = true })
-  },
-  {
-    key = "w",
-    mods = "LEADER",
-    action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" })
-  },
-  {
-    key = "s",
-    mods = "LEADER",
-    action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" })
-  }
-}
-
-smart_splits.apply_to_config(config)
 
 return config
