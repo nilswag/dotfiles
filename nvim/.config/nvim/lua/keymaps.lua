11fd4557323996d93b151847a3dbbea177fefafa
nilswag <nilswagemaker05@icloud.com>
2026-06-24 19:13:14 +0200

    Removed and added some keybinds

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -3,7 +3,6 @@ vim.g.mapleader = " "
 -- replaces selected text without losing what you yanked
 vim.keymap.set("x", "p", [["_dP]], { desc = "Paste over selection without losing yanked text" })
 
-vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Shortcut for going from insert to normal mode" })
 vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search highlighting", silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move lines down in visual selection" })
@@ -14,3 +13,5 @@ vim.keymap.set("v", ">", ">gv", { desc = "Indent and keep selection" })
 
 vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Move down in buffer with cursor centered" })
 vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Move up in buffer with cursor centered" })
+
+vim.keymap.set("n", "<leader>re", ":restart<CR>", { desc = "Restart neovim", silent = true })
