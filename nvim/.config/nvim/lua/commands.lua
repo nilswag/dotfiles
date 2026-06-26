623694fb011f32b35686c584dc39c81eeb859975
nilswag <nilswagemaker05@icloud>
2026-06-26 12:41:18 +0200

    Added PackUpdate custom neovim command

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -5,3 +5,12 @@ end, { nargs = "+", desc = "Add plugins (:PackAdd user/repo)" })
 vim.api.nvim_create_user_command("PackDelete", function(opts)
     vim.pack.del(opts.fargs)
 end, { nargs = "+", desc = "Delete plugins (:PackDel plugin1 plugin2)" })
+
+vim.api.nvim_create_user_command("PackUpdate", function(opts)
+    if opts.args:match("%S") then
+        local plugins = vim.split(opts.args, "%s+", { trimempty = true })
+        vim.pack.update(plugins)
+    else
+        vim.pack.update()
+    end
+end, { nargs = "*", desc = "Update all plugins or specific ones" })
