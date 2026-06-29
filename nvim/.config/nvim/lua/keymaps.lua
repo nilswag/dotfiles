56c05caf149486262010c4f2671a7b14d1a042f8
nilswag <nilswagemaker05@icloud.com>
2026-06-29 15:17:47 +0200

    Added more keybinds

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -4,18 +4,30 @@
 vim.g.mapleader = " " -- space for leader
 vim.g.maplocalleader = " " -- space for local leader
 
-vim.keymap.set("n", "<leader>c", ":nohlsearch<cr>", { desc = "Clear search highlights" })
+vim.keymap.set("n", "<Leader>c", ":nohlsearch<cr>", { desc = "Clear search highlights" })
 
 vim.keymap.set("n", "n", "nzz", { desc = "Next search result (centered)" })
 vim.keymap.set("n", "N", "Nzz", { desc = "Previous search result (centered)" })
-vim.keymap.set("n", "<c-d>", "<c-d>zz", { desc = "Half page down (centered)" })
-vim.keymap.set("n", "<c-u>", "<c-u>zz", { desc = "Half page up (centered)" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
 
-vim.keymap.set("n", "<leader>bn", ":bnext<cr>", { desc = "Next buffer" })
-vim.keymap.set("n", "<leader>bp", ":bprevious<cr>", { desc = "Previous buffer" })
-vim.keymap.set("n", "<c-tab>", ":bnext<cr>", { desc = "Next buffer (cycle)" })
+vim.keymap.set("n", "<Leader>bn", ":bnext<cr>", { desc = "Next buffer" })
+vim.keymap.set("n", "<Leader>bp", ":bprevious<cr>", { desc = "Previous buffer" })
+vim.keymap.set("n", "<C-tab>", ":bnext<cr>", { desc = "Next buffer (cycle)" })
 
-vim.keymap.set("n", "<c-h>", "<C-w>h", { desc = "Move to left window" })
-vim.keymap.set("n", "<c-l>", "<C-w>l", { desc = "Move to right window" })
-vim.keymap.set("n", "<c-j>", "<C-w>j", { desc = "Move to bottom window" })
-vim.keymap.set("n", "<c-k>", "<C-w>k", { desc = "Move to top window" })
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
+
+vim.keymap.set("n", "<Leader>sv", ":vsplit<cr>", { desc = "Split vertically" })
+vim.keymap.set("n", "<Leader>sh", ":split<cr>", { desc = "Split horizontally" })
+vim.keymap.set("n", "<C-down>", ":resize -2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-up>", ":resize +2<cr>", { desc = "Increase window height" })
+vim.keymap.set("n", "<C-left>", ":vertical resize +2<cr>", { desc = "Increase window width" })
+vim.keymap.set("n", "<C-right>", ":vertical resize -2<cr>", { desc = "Increase window width" })
+
+vim.keymap.set("n", "<A-j>", ":m .+1<cr>==", { desc = "Move line down" })
+vim.keymap.set("n", "<A-k>", ":m .-2<cr>==", { desc = "Move line up" })
+vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move selection up" })
+vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move selection down" })
--- nvim/.config/nvim/lua/plugins/mini-pick.lua
+++ nvim/.config/nvim/lua/plugins/mini-pick.lua
@@ -4,9 +4,9 @@ M.config = function()
     local mini_pick = require("mini.pick")
     mini_pick.setup()
 
-    vim.keymap.set("n", "<leader>pf", function() mini_pick.builtin.files() end, { desc = "Pick files" })
-    vim.keymap.set("n", "<leader>ps", function() mini_pick.builtin.grep({ pattern = vim.fn.expand("<cword>") }) end, { desc = "Pick word" })
-    vim.keymap.set("n", "<leader>vh", function() mini_pick.builtin.help() end, { desc = "Pick help" })
+    vim.keymap.set("n", "<Leader>pf", function() mini_pick.builtin.files() end, { desc = "Pick files" })
+    vim.keymap.set("n", "<Leader>ps", function() mini_pick.builtin.grep({ pattern = vim.fn.expand("<cword>") }) end, { desc = "Pick word" })
+    vim.keymap.set("n", "<Leader>vh", function() mini_pick.builtin.help() end, { desc = "Pick help" })
 end
 
 return M
